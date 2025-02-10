e0891846b949bebad1e775fd9deaf6c24ea2482a
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-09 21:33:39 -0300

    Agrega componente ListOfEmployees y EmployeeService al frontend

---
+++ ems-frontend/src/components/ListEmployeeComponents.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { listEmployees } from "../services/EmployeeService";
+import "../styles/styles.css";
+const ListEmployeeComponents = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="text-center p-4">List of Employees</h2>
+      <table className="table table-striped table-dark table-hover table-rounded">
+        <thead>
+          <tr>
+            <th className="w-25 p-3 fs-5 text-center">ID</th>
+            <th className="w-25 p-3 fs-5 text-center">First Name</th>
+            <th className="w-25 p-3 fs-5 text-center">Last Name</th>
+            <th className="w-25 p-3 fs-5 text-center">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListEmployeeComponents;
